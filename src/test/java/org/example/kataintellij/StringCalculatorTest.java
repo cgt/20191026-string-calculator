ea35d82658db8af857b1366979b1526fca258e0c
Christoffer G. Thomsen <chris@cgt.name>
2019-10-26 18:41:59 +0200

    working
    Co-authored-by: Scott Jaderholm <jaderholm@gmail.com>

--- src/test/java/org/example/kataintellij/StringCalculatorTest.java
+++ src/test/java/org/example/kataintellij/StringCalculatorTest.java
@@ -14,15 +14,21 @@ public class StringCalculatorTest {
         assertEquals(0, add(""));
     }
 
-
     @Test
     public void given_1_returns_1() {
         assertEquals(1, add("1"));
     }
 
+    @Test
+    public void given_2_returns_2() {
+        assertEquals(2, add("2"));
+    }
+
     private int add(String s) {
         if (s.equals("1")) {
             return 1;
+        } else if(s.equals("2")) {
+            return 2;
         }
         return 0;
     }
