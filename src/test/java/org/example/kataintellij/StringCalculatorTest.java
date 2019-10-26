43bc66ff19d0b602260e4f1c2f957a4f2dec2dbe
Christoffer G. Thomsen <chris@cgt.name>
2019-10-26 18:36:40 +0200

    working
    Co-authored-by: Scott Jaderholm <jaderholm@gmail.com>

--- src/test/java/org/example/kataintellij/StringCalculatorTest.java
+++ src/test/java/org/example/kataintellij/StringCalculatorTest.java
@@ -2,8 +2,20 @@ package org.example.kataintellij;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringCalculatorTest {
     @Test
     public void nothing() {
     }
+
+    @Test
+    public void emptyStringReturnsZero() {
+        assertEquals(0, add(""));
+    }
+
+    private int add(String s) {
+        return 0;
+    }
+
 }
