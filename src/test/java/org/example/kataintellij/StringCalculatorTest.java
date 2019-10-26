37b2563ac685ebdbf756beca5ad8692e43c2d243
Christoffer G. Thomsen <chris@cgt.name>
2019-10-26 18:47:35 +0200

    working
    Co-authored-by: Scott Jaderholm <jaderholm@gmail.com>

--- src/test/java/org/example/kataintellij/StringCalculatorTest.java
+++ src/test/java/org/example/kataintellij/StringCalculatorTest.java
@@ -13,6 +13,7 @@ public class StringCalculatorTest {
 
     @Test
     public void given_any_one_single_digit_returns_same_digit() {
+        assertEquals(0, add("0"));
         assertEquals(1, add("1"));
         assertEquals(2, add("2"));
         assertEquals(3, add("3"));
